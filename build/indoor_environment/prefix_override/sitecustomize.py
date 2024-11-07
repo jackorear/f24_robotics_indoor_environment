253ac7f9e01149a6e30b50849a5d5892e7e96d83
jackorear <jorear@crimson.ua.edu>
2024-11-07 17:38:08 -0600

    pushing working package with outdoor env

--- build/indoor_environment/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jorear/home/f24_robotics_indoor_environment/install/indoor_environment'
